769f25da53f8cf8f654df7fc26b3f50b7e1e5911
Владимир Бойко <E>
2018-05-08 15:42:20 +0300

    half of chapter 07

--- index.js
+++ index.js
@@ -3,7 +3,45 @@ const fortunes = require('./lib/fortune');
 
 const app = express();
 
-const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+const handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  helpers: {
+    section: (name, options) => {
+      if (!this._sectoions) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    },
+  },
+});
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Портленд',
+        forecastURL: 'http://www.wunderground.com/US/OR/portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Сплошная облачность',
+        temp: '54.1 f (12.3 C)',
+      },
+      {
+        name: 'Бенд',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Малооблачно',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Манзанита',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Небольшой дождь',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -11,6 +49,11 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+app.use((req, res, next) => {
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
   next();
 });
 app.get('/', (req, res) => {
